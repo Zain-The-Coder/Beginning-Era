abd1a17c50ffa41640e40857fb9f13ff0f864f64
Zain-The-Coder <zain015976@gmail.com>
2025-06-21 00:01:30 +0500

    practise javascript

---
+++ Javascript/Beginning level/app.js
@@ -0,0 +1,28 @@
+// let firstName = "Zain ";
+// let middleName = "Ur ";
+// let lastName = "Rehman ";
+// console.log(firstName + middleName + lastName);
+
+// prompt("Enter your name");
+
+// let num1 = 2;
+// let num2 = 4;
+// let num3 = 8;
+// let num4 = 13;
+
+// let sum = `${num2 + num4}${num4}`;
+// console.log(sum);
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let sum = b*(c + a) + d*(b + c)*(b + a);
+console.log (sum);
+
+// let names = prompt("What is your name?");
+// let fatherName = prompt("What is your father name ?");
+// let profession = prompt("What is your profession?");
+// alert("Welcome on Zain's Page"); 
+// alert("Registration Successful");
+// console.log("Welcome " + names  + " Your father name is " + fatherName  + " Your profession is " + profession);
