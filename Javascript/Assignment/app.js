246e4d874b8efe0af84c11636af67ce033a01fee
Zain-The-Coder <zain015976@gmail.com>
2025-07-01 16:02:23 +0500

    Practising JS

--- Javascript/Assignment/app.js
+++ Javascript/Assignment/app.js
@@ -43,6 +43,43 @@
 // let f = (c *  9 / 5) + 32 ;
 // console.log("Your Celcius Temperature is equals to " + f + " Farenheit");
 
-let f = prompt("Enter a temperature in Farenheit :");
-let c = ( f - 32 ) * 5 / 9 ;
-console.log("Your Farenheit temperature is equals to " + c + " Celcius"); 
+// let f = prompt("Enter a temperature in Farenheit :");
+// let c = ( f - 32 ) * 5 / 9 ;
+// console.log("Your Farenheit temperature is equals to " + c + " Celcius"); 
+
+// FIFTH TASK
+
+// let item1Price = 200;
+// let item2Price = 400;
+// let quanItem1 = +prompt("Enter The Quantity of Item 1 :");
+// let quanItem2 = +prompt("Enter The Quantity of Item 2 :");
+// let billItem1 = (item1Price * quanItem1);
+// let billItem2 = (item2Price * quanItem2);
+// let totalBil = (quanItem1 * item1Price) + (quanItem2 * item2Price);
+// let sc = (totalBil / 100 * 5);
+// let totalBill = totalBil + sc;
+
+// console.log("Price of Item 1 is " + item1Price);
+// console.log("Quantity of Item 1 is " + quanItem1);
+// console.log("Total Bill of Item 1 is " + billItem1);
+// console.log("Price of Item 2 is " + item2Price);
+// console.log("Quantity of Item 2 is " + quanItem2);
+// console.log("Total Bill of Item 2 is " + billItem2);
+// console.log("Shipping Charges is " + sc);
+// console.log("Your Total Bill Is : " + totalBill);
+
+
+
+// SIXTH TASK
+
+// let tno = prompt("Enter Total Marks :");
+// let obt = prompt("Enter Your Total Obtained Marks :");
+// let per = (obt * 100) / tno;
+// console.log("Total Numbers Are : " + tno);
+// console.log("Marks Obtained By Student " + obt);
+// console.log("Student Percentage is " + per);
+
+// SEVENTH TASK
+
+
+
