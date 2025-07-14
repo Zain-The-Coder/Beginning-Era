4eafd577072b866e94f7fa6da6f8488ff7495c43
Zain-The-Coder <zain015976@gmail.com>
2025-07-14 23:35:17 +0500

    JS

---
+++ Javascript/Loop/app.js
@@ -0,0 +1,37 @@
+// let tno = prompt("Enter Your Table Number :");
+// for (let i = 1 ; i <= 10 ; i++) {
+//     console.log(tno + " x " + i + " = " + tno*i);
+// }
+
+// let userInput = prompt("");
+// for (let i = 0 ; i <= userInput ; i++) {
+//     console.log(i);
+// }
+
+
+// let loopStart = prompt("Enter Your Starting Point Of Loop :");
+// let loopEnd = prompt("Enter Your Ending Point Of Loop :");
+// for (let i = loopStart ; i <= loopEnd ; i++) {
+//     console.log("Chai Ly Aao !" , i);
+// }
+
+
+// let loopStart = +prompt("Enter Your Starting Point Of Loop :");
+// let loopEnd = +prompt("Enter Your Ending Point Of Loop :");
+// let skip = +prompt("How many numbers to skip ?");
+// for (let i = loopStart ; i <= loopEnd ; i = i + skip){
+//     console.log(i);
+// }
+
+// let fruits = [" Apple ", " Banana " , " Orange " , " Mango "];
+// for(let i = 0 ; i < fruits.length ; i++) {
+//     console.log("Your List Of Fruits Is :");
+//     console.log(fruits)
+// }
+
+// let seasons = ["s1" , "s2" , "s3" , "s4" , "s5" , "s6" , "s7" , "s8" , "s9"];
+// for (let i = 0 ; i < seasons.length ; i++) {
+//     console.log(seasons[i]);
+// }
+
+
