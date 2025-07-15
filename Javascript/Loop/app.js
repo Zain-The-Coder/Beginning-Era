b526424e0b8ec4a947b6189a6fb501efca7d0b3c
Zain-The-Coder <zain015976@gmail.com>
2025-07-15 17:20:56 +0500

    JS

--- Javascript/Loop/app.js
+++ Javascript/Loop/app.js
@@ -53,6 +53,48 @@
 // console.log("Total Even Numbers Are : " + evenNumber);
 
 
-let numbers = [1,2,3,4,5,6,7,4,77,56,77,77];
-let total = 0;
+// let fruits = ["apple" , " banana " , " pineapple " , " gavava " , "strawberry"];
+// let extraFruit = fruits.unshift("mango");
+// for (i = 0 ; i <= fruits.length -1 ; i++) {
+//     console.log(fruits[i]);
+// }
 
+// let numbers = [2 , 6 , 4 , 67 , 32 , 78 , 7 , 99 , 53 , 55];
+// let evenNumbers = 0;
+// let oddNumbers = 0;
+// for (i = 0 ; i <numbers.length ; i++) {
+//    if(numbers[i] % 2 === 0) {
+//     evenNumbers = evenNumbers + numbers[i] ;
+//    }
+//    else {
+//     oddNumbers = oddNumbers + numbers[i];
+//    }
+// }
+// console.log(evenNumbers);
+// console.log(oddNumbers)
+
+// let numbers = [2 , 6 , 4 , 67 , 32 , 78 , 7 , 99 , 53 , 97 , 55];
+// let evenNumber = 0;
+// let oddNumber = 0;
+// for (i = 0 ; i < numbers.length ; i++) {
+//     if(numbers[i] % 2 === 0) {
+//         evenNumber++
+//     } else {
+//         oddNumber++
+//     }
+// }
+// console.log(evenNumber); console.log(oddNumber)
+
+let numbers = [2 , 6 , 4 , 67 , 32 , 78 , 7 , 99 , 53 , 97 , 55];
+let evenNumber = 0 ;
+let oddNumber = 0 ;
+
+for (i = 0 ; i < numbers.length ; i++) {
+    if(numbers[i] % 2 === 0) {
+        evenNumber = evenNumber + numbers[i];
+    } else {
+        oddNumber = oddNumber + numbers[i]
+    }
+}
+console.log(evenNumber);
+console.log(oddNumber)
