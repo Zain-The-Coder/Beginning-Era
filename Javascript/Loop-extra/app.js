da97c55146c8c0c64873b5b27b0235edaf74c4d1
Zain-The-Coder <zain015976@gmail.com>
2025-07-18 01:06:17 +0500

    JS

--- Javascript/Loop-extra/app.js
+++ Javascript/Loop-extra/app.js
@@ -1,27 +1,53 @@
-let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
-let studentMarks = [88, 95, 67, 32, 76, 45];
-let totalMarks = 100;
-let percentage = [];
-let grades = [] ;
-for (i = 0; i < studentNames.length; i++) {
-    if(studentMarks[i] >= 90) {
-        grades.push("A+") ;
-        percentage.push((studentMarks[i] / totalMarks) * 100  + "%");
-    } else if(studentMarks[i] >= 80) {
-        grades.push("A");
-        percentage.push((studentMarks[i] / totalMarks) * 100  + "%");
-    } else if(studentMarks[i] >= 70) {
-        grades.push("B");
-        percentage.push((studentMarks[i] / totalMarks) * 100  + "%");
-    } else if(studentMarks[i] >= 60) {
-        grades.push("C");
-        percentage.push((studentMarks[i] / totalMarks) * 100  + "%");
-    } else if(studentMarks[i] >= 50) {
-        grades.push("D");
-        percentage.push((studentMarks[i] / totalMarks) * 100  + "%");
-    } else {
-        grades.push("Fail");
-        percentage.push((studentMarks[i] / totalMarks) * 100  + "%");
-    } 
-} 
-console.log("Name : " + studentNames + " \n " + "Grades : " + grades + ("\n") + " percentage " + percentage)
+// let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
+// let studentMarks = [88, 95, 67, 32, 76, 45];
+// let totalMarks = 100;
+// let lastResult = [] ;
+// for (i = 0; i < studentNames.length; i++) {
+//     let percentage = ((studentMarks[i] / totalMarks) * 100);
+//     let grade = "" ;
+//     if(percentage >= 90) {
+//         grade = "A+" ;
+//     } else if(percentage >= 80) {
+//         grade = "A" ;
+//     } else if(percentage >= 70) {
+//         grade = "B" ;
+//     } else if(percentage >= 60) {
+//         grade = "C" ;
+//     } else if(percentage >= 50) {
+//         grade = "D" ;
+//     } else {
+//         grade = "Fail" ;
+//     }
+//     lastResult.push(studentNames[i] + " - Marks : " + studentMarks[i] + " - Percentage : " + percentage + "%" 
+//         + " - Grade : " + grade);
+// }
+// console.log("STUDNETS RESULT");
+// console.log(lastResult.join("\n"));
+
+
+// let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
+// let studentMarks = [88, 95, 67, 92, 76, 90];
+// let totalMarks = 100;
+// scholarShipResult = [] ;
+// for (i = 0; i < studentNames.length; i++) {
+//     let percentage = ((studentMarks[i] / totalMarks) * 100);
+//     let result = "" ;
+//     if(percentage >= 85) {
+//         result = "You Are Eligible For Scholarship !";
+//     } else {
+//         result = "You Are Not Eligible For Scholarship !";
+//     }
+//     scholarShipResult.unshift(studentNames[i] + " - " +"Percentage : " + percentage + "%" + " - " + result);
+// }
+// console.log(scholarShipResult.join("\n"));
+
+let firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+let lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+let fullNames = [];
+let mm = 0;
+for (var i = 0; i < firstNames.length; i++) {
+fullNames.push(firstNames[i] + (mm = mm + lastNames[i]));
+}
+console.log(fullNames)
+
+
