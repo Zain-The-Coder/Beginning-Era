415f1518a136823ed43150ed0ae2d38d857a91bf
Zain-The-Coder <zain015976@gmail.com>
2025-07-19 02:16:43 +0500

    'JS

--- Javascript/Loop-extra/app.js
+++ Javascript/Loop-extra/app.js
@@ -1,83 +1,116 @@
-// let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
-// let studentMarks = [88, 95, 67, 32, 76, 45];
-// let totalMarks = 100;
-// let lastResult = [] ;
-// for (i = 0; i < studentNames.length; i++) {
-//     let percentage = ((studentMarks[i] / totalMarks) * 100);
-//     let grade = "" ;
+// // let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
+// // let studentMarks = [88, 95, 67, 32, 76, 45];
+// // let totalMarks = 100;
+// // let lastResult = [] ;
+// // for (i = 0; i < studentNames.length; i++) {
+// //     let percentage = ((studentMarks[i] / totalMarks) * 100);
+// //     let grade = "" ;
+// //     if(percentage >= 90) {
+// //         grade = "A+" ;
+// //     } else if(percentage >= 80) {
+// //         grade = "A" ;
+// //     } else if(percentage >= 70) {
+// //         grade = "B" ;
+// //     } else if(percentage >= 60) {
+// //         grade = "C" ;
+// //     } else if(percentage >= 50) {
+// //         grade = "D" ;
+// //     } else {
+// //         grade = "Fail" ;
+// //     }
+// //     lastResult.push(studentNames[i] + " - Marks : " + studentMarks[i] + " - Percentage : " + percentage + "%" 
+// //         + " - Grade : " + grade);
+// // }
+// // console.log("STUDNETS RESULT");
+// // console.log(lastResult.join("\n"));
+
+
+// // let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
+// // let studentMarks = [88, 95, 67, 92, 76, 90];
+// // let totalMarks = 100;
+// // scholarShipResult = [] ;
+// // for (i = 0; i < studentNames.length; i++) {
+// //     let percentage = ((studentMarks[i] / totalMarks) * 100);
+// //     let result = "" ;
+// //     if(percentage >= 85) {
+// //         result = "You Are Eligible For Scholarship !";
+// //     } else {
+// //         result = "You Are Not Eligible For Scholarship !";
+// //     }
+// //     scholarShipResult.unshift(studentNames[i] + " - " +"Percentage : " + percentage + "%" + " - " + result);
+// // }
+// // console.log(scholarShipResult.join("\n"));
+
+
+// // let firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// // let lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// // let fullNames = [];
+// // let mm = 0;
+// // for (var i = 0; i < firstNames.length; i++) {
+// // fullNames.push(firstNames[i] + lastNames[i]);
+// // }
+// // console.log(fullNames);
+
+
+// // let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
+// // let studentMarks = [88, 95, 67, 92, 76, 90];
+// let studentData = [ 
+//     { Name : "Ali" , Marks : 88 ,},
+//     { Name : "Sara" , Marks : 95 ,},
+//     { Name : "Zain" , Marks : 67 ,},
+//     { Name : "Hina" , Marks : 92 ,},
+//     { Name : "Ahmed" , Marks : 76 ,},
+//     { Name : "Fatima" , Marks : 90 ,}
+// ]
+// let totalMarks = 100 ;
+// for (let i = 0; i <studentData.length; i++) {
+//     let percentage = ((studentData[i].Marks / totalMarks) * 100);
+//     studentData[i].percentage = percentage.toFixed(2);
 //     if(percentage >= 90) {
-//         grade = "A+" ;
+//         studentData[i].grade = "A+" ;
 //     } else if(percentage >= 80) {
-//         grade = "A" ;
+//         studentData[i].grade = "A";
 //     } else if(percentage >= 70) {
-//         grade = "B" ;
+//         studentData[i].grade = "B" ;
 //     } else if(percentage >= 60) {
-//         grade = "C" ;
+//         studentData[i].grade = "C" ;
 //     } else if(percentage >= 50) {
-//         grade = "D" ;
+//         studentData[i].grade = "D" ;
 //     } else {
-//         grade = "Fail" ;
+//         studentData[i].grade = "Fail";
 //     }
-//     lastResult.push(studentNames[i] + " - Marks : " + studentMarks[i] + " - Percentage : " + percentage + "%" 
-//         + " - Grade : " + grade);
+// console.log(studentData[i].Name + " - Marks : " + studentData[i].Marks + " - Percentage : " + studentData[i].percentage + " - Grade : " + studentData[i].grade);
 // }
-// console.log("STUDNETS RESULT");
-// console.log(lastResult.join("\n"));
 
+let employees = [
+    { name: "Usman", score: 78 },
+    { name: "Areeba", score: 91 },
+    { name: "Kashif", score: 65 },
+    { name: "Nimra", score: 45 },
+    { name: "Bilal", score: 84 },
+    { name: "Huda", score: 99 }
+];
 
-// let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
-// let studentMarks = [88, 95, 67, 92, 76, 90];
-// let totalMarks = 100;
-// scholarShipResult = [] ;
-// for (i = 0; i < studentNames.length; i++) {
-//     let percentage = ((studentMarks[i] / totalMarks) * 100);
-//     let result = "" ;
-//     if(percentage >= 85) {
-//         result = "You Are Eligible For Scholarship !";
-//     } else {
-//         result = "You Are Not Eligible For Scholarship !";
-//     }
-//     scholarShipResult.unshift(studentNames[i] + " - " +"Percentage : " + percentage + "%" + " - " + result);
-// }
-// console.log(scholarShipResult.join("\n"));
+let totalScore = 100;
 
-
-// let firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
-// let lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
-// let fullNames = [];
-// let mm = 0;
-// for (var i = 0; i < firstNames.length; i++) {
-// fullNames.push(firstNames[i] + lastNames[i]);
-// }
-// console.log(fullNames);
-
-
-// let studentNames = ["Ali", "Sara", "Zain", "Hina", "Ahmed", "Fatima"];
-// let studentMarks = [88, 95, 67, 92, 76, 90];
-let studentData = [ 
-    { Name : "Ali" , Marks : 88 ,},
-    { Name : "Sara" , Marks : 95 ,},
-    { Name : "Zain" , Marks : 67 ,},
-    { Name : "Hina" , Marks : 92 ,},
-    { Name : "Ahmed" , Marks : 76 ,},
-    { Name : "Fatima" , Marks : 90 ,}
-]
-let totalMarks = 100 ;
-for (let i = 0; i <studentData.length; i++) {
-    let percentage = ((studentData[i].Marks / totalMarks) * 100);
-    studentData[i].percentage = percentage.toFixed(2);
+for (let i = 0; i < employees.length; i++) {
+    let percentage = ((employees[i].score / totalScore) * 100);
+    employees[i].percentage = percentage.toFixed(2);
     if(percentage >= 90) {
-        studentData[i].grade = "A+" ;
+        employees[i].grade = "⭐ Excellent " ;
     } else if(percentage >= 80) {
-        studentData[i].grade = "A";
+        employees[i].grade = " 🥇 Very Good " ;
     } else if(percentage >= 70) {
-        studentData[i].grade = "B" ;
+        employees[i].grade = " 🥈 Good " ;
     } else if(percentage >= 60) {
-        studentData[i].grade = "C" ;
+        employees[i].grade = " 🟡 Average " ;
     } else if(percentage >= 50) {
-        studentData[i].grade = "D" ;
+        employees[i].grade = "⚠️ Needs Improvement " ;
     } else {
-        studentData[i].grade = "Fail";
+        employees[i].grade = " ❌ Poor "
     }
-console.log(studentData[i].Name + " - Marks : " + studentData[i].Marks + " - Percentage : " + studentData[i].percentage + " - Grade : " + studentData[i].grade);
+    console.log(employees[i].name + " - Score : " + employees[i].score + " - Percentage : " 
+        + percentage + "%" + " - Grade : " + employees[i].grade);
 }
+// console.log(result);
+// https://youtu.be/B2AySOOwWJY?si=QmxjnzS_bpmQZqB5 ;
