7243da2481e6ebe4159828eef2e4c2ed5cb81202
Zain-The-Coder <zain015976@gmail.com>
2025-07-29 15:09:54 +0500

    JS

--- Javascript/Numbers/app.js
+++ Javascript/Numbers/app.js
@@ -1,31 +1,85 @@
-console.log(Math.random());
+// console.log(Math.random());
 
-console.log(Math.random() * 10);
+// console.log(Math.random() * 20);
 
-console.log((Math.random() * 5 ) + 1 );
+// console.log((Math.random() * 5 ) + 1 );
 
-console.log(Math.round(Math.random() * 5 ) + 1 );
+// console.log(Math.round(Math.random()));
 
-console.log(Math.round(65.3));
-console.log(Math.round(65.6));
-console.log(Math.round(59.5));
+// console.log(Math.round(65.3));
+// console.log(Math.round(65.6));
+// console.log(Math.round(59.5));
 
-console.log(Math.floor(23.3));
-console.log(Math.floor(56.7));
-console.log(Math.floor(98.9));
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(56.7));
+// console.log(Math.floor(98.9));
 
-console.log(Math.ceil(56.1));
-console.log(Math.ceil(45.6));
+// console.log(Math.ceil(56.1));
+// console.log(Math.ceil(45.6));
 
-let cal = Math.floor(Math.random() * 10) + 1;
-console.log(cal);
+// let cal = Math.floor(Math.random() * 10) + 1;
+// console.log(cal);
 
 
-let calc = Math.ceil(Math.random() * 10) + 1;
-console.log(calc);
+// let calc = Math.ceil(Math.random() * 10) + 1;
+// console.log(calc);
 
-let dice = Math.floor(Math.random() * 6) ;
-console.log(dice);
+// let dice = Math.floor(Math.random() * 6) ;
+// console.log(dice);
 
-let dic = Math.ceil(Math.random() * 10);
-console.log(dic);
+// let dic = Math.ceil(Math.random() * 10);
+// console.log(dic);
+
+// let toss = Math.random() ;
+// if(toss > 0.5) {
+//     console.log("Heads !");
+// } else {
+//     console.log("Tails !");
+// }
+
+
+// let a = 56;
+// let b = 43;
+// let c = 12;
+// console.log(Math.min(a , b , c));
+// console.log(Math.min(34 , 45 , 76));
+
+// console.log(Math.max(32 , 45 , 21));
+// console.log(Math.max(a , b , c));
+
+// if(a > b && a > c) {
+//     console.log(a);
+// } else if(b > a && b > c) {
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
+
+// console.log(Math.abs(6));
+// console.log(Math.abs(-43));
+
+// let number = 54 ;
+// let num = 43.5;
+// console.log(String(number) + 4);
+
+// console.log(String(num));
+
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.random(num));
+
+// let n = Math.ceil(Math.random() * 10); 
+// console.log(n);
+// console.log(Math.floor(Math.random()) + 1)
+
+// console.log(Math.abs(-32.4));
+
+// let number = 34;
+// console.log(Number(number) + 3);   // Make addition
+// console.log(String(number) + 3);   // concatinate
+// console.log((Number(number) + 3).toString()); // additon but turn number into string
+
+let otp = Math.floor((Math.random() * 9000) + 1000);
+console.log(otp);
+
