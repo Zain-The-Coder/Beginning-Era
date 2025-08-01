582a68b54c0c0c64692d1219050ced41bed15a9a
Zain-The-Coder <zain015976@gmail.com>
2025-08-01 14:50:32 +0500

    JS

--- Javascript/Assignment/app.js
+++ Javascript/Assignment/app.js
@@ -1,137 +1,69 @@
-// Assignment No : 05
+// ASSIGNMENT NO 1 
+// JAVASCRIPT BASICS 
 
-// First task
+// Q1 :
+// let n = prompt("Enter your name :")
+// alert("Hello " + n);
 
-// let x = +prompt("Enter 1st Number : ");
-// let y = +prompt("Enter 2nd Number :");
+//Q2 :
+// let f = prompt("Enter your first name :");
+// let l = prompt("Enter your last name :");
+// alert("Assalam u Alaikum " + f + " " + l);
 
-// let z = ( x + y);
-// let za = (x - y);
-// let zai = (x * y);
-// let zain = (x / y);
-// console.log("The Addition Of Two Numbers Is : " + z);
-// console.log("The Substraction Of Two Numbers Is :" + za);
-// console.log("The Multiplication Of Two Numbers Is : " + zai);
-// console.log("The Division Of Two Numbers is : " + zain);
-
-// Second Task
-
-// let ticket = 600;
-// let ticketQuantity = prompt("Enter A Buying Quantity Of Ticket :");
-// console.log("Total Cost To Buy " + ticketQuantity + " tickets to a movie is "
-//     + (ticket * ticketQuantity) + "PKR");
-
-
-// // Third Task
-
-// let tno = prompt("Enter A Table Number :");
-// let i = 0;
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-// console.log(tno + " x " + ++i + " = " + tno*i);
-
-// Fourth Task
-
-// let c = prompt("Enter temperature In Celcius :");
-// let f = (c *  9 / 5) + 32 ;
-// console.log("Your Celcius Temperature is equals to " + f + " Farenheit");
-
-// let f = prompt("Enter a temperature in Farenheit :");
-// let c = ( f - 32 ) * 5 / 9 ;
-// console.log("Your Farenheit temperature is equals to " + c + " Celcius"); 
-
-// FIFTH TASK
-
-// let item1Price = 200;
-// let item2Price = 400;
-// let quanItem1 = +prompt("Enter The Quantity of Item 1 :");
-// let quanItem2 = +prompt("Enter The Quantity of Item 2 :");
-// let billItem1 = (item1Price * quanItem1);
-// let billItem2 = (item2Price * quanItem2);
-// let totalBil = (quanItem1 * item1Price) + (quanItem2 * item2Price);
-// let sc = (totalBil / 100 * 5);
-// let totalBill = totalBil + sc;
-
-// console.log("Price of Item 1 is " + item1Price);
-// console.log("Quantity of Item 1 is " + quanItem1);
-// console.log("Total Bill of Item 1 is " + billItem1);
-// console.log("Price of Item 2 is " + item2Price);
-// console.log("Quantity of Item 2 is " + quanItem2);
-// console.log("Total Bill of Item 2 is " + billItem2);
-// console.log("Shipping Charges is " + sc);
-// console.log("Your Total Bill Is : " + totalBill);
+//Q3 :
+// for(let i = 0; i < 2 ; ++i) {
+//     let age = prompt("Enter your age :");
+//     if(isNaN(age)) {
+//         alert("please enter valid age or in numbers !");
+//         continue ;
+//     } else {
+    //     alert("you are " + age + " years old");
+    //     }
+    // }
+    
+// ASSIGNMENT NO 2 
+// WORKING WITH MATH AND CONDITION 
 
 
+// Q1 and Q2 : 
+// let operations = ["add" , "sub" , "mul" , "div" , "rem"] 
+//     let x = +prompt("enter first number :");
+//     let y = +prompt("enter second number :");
+//     let useroper = prompt("which arithematic operation you want to apply numbers :");
+//     let updatedOper = useroper.toLowerCase().slice(0 , 3);
+//     if(updatedOper === operations[0]) {
+//         alert("the addition of your numbers is : " + (x + y));
+//     }
+//     if(updatedOper === operations[1]) {
+//         alert("the addition of your numbers is : " + (x - y));
+//     }if(updatedOper === operations[2]) {
+//         alert("the addition of your numbers is : " + (x * y));
+//     }if(updatedOper === operations[3]) {
+//         alert("the addition of your numbers is : " + (x / y));
+//     }
+//     if(updatedOper === operations[4]) {
+//         alert("the moodulus remain from your numbers is : " + (x % y));
+//     }
 
-// SIXTH TASK
+// Q2 REMAINING 
+// let userNumber = prompt("enter your number between 1 to 100") ;
+// for(let i = userNumber; i <= 200 ; i++) {
+//     alert("increment is going on " + i);
+//     if(i === 200) {
+//         alert("YOU REACHED THE LIMIT !");
+//     }
+// }
+// let userNumber = prompt("enter your number between 100 to 200") ;
+// for(let i = userNumber; i >= -50 ; i--) {
+//     alert("decrement is going on " + i);
+//     if(i === -50) {
+//         alert("YOU REACHED THE LIMIT !");
+//     }
+// }
 
-// let tno = prompt("Enter Total Marks :");
-// let obt = prompt("Enter Your Total Obtained Marks :");
-// let per = (obt * 100) / tno;
-// console.log("Total Numbers Are : " + tno);
-// console.log("Marks Obtained By Student " + obt);
-// console.log("Student Percentage is " + per);
+//Q3 :
 
-// SEVENTH TASK
 
-// let dollar = 283;
-// let riyal = 75;
-// let pkrDollar = prompt("Enter, How Much Dollar You Have :");
-// let pkrRiyal = prompt("Enter, How Much Saudi Riyal You have :")
-// let pkrD = pkrDollar * 283;
-// let pkrR = pkrRiyal * 75;
-// let total = pkrD + pkrR;
-// console.log("According to Convert Your US Dollars, " + "You Have " + pkrD + "PKR");
-// console.log("According to Convert Your Saudi Riyal, " + "You Have " + pkrR + "PKR");
-// console.log("You have Total Currency is " + total + "PKR");
-
-// EIGHT TASK 
-
-// let x = +prompt("Enter Any Number : ");
-// let add = x + 5;
-// let mul = add * 10;
-// let result = mul / 2;
-// console.log("The Required Output is : " + result);
-// console.log("In Above , Your number is add by 5 and then multiply by 10 and answer divided by 2");
-
-//  NINTH TASK 
-
-// let cy = 2025;
-// let by = prompt("Enter Your Year Of Birth :");
-// console.log("Currunt Year : " + cy);
-// console.log("Your Birth Year : " + by);
-// let x = (cy- by);
-// console.log("Your Age Is : " + x);
-
-// TENTH TASK
-
-// let radius = prompt("Enter The Radius Of Circle : ");
-// let π = 3.142;
-// let circum = (2 * π * radius);
-// let area = (π * radius * radius);
-// console.log("Radius Of A Circle Is :" + radius);
-// console.log("Circumference Of A Circle Is :" + circum);
-// console.log("The Area Of A Circle Is : " + area);
-
-// ELEVENTH TASK
-
-// let favSnack = prompt("Enter Your Favorite Snack :");
-// let curAge = prompt("Enter Your Age : ");
-// let maxAge = 65;
-// let dailyAvg = prompt("How Much You Eat Your Favorite Snack In A Day :");
-// let age = (maxAge - curAge);
-// let day = 365;
-// let a = (age * day);
-// let answer = (a * dailyAvg);
-// console.log("You Will Need " + answer + " " +  favSnack + " According to your daily need " 
-//     +  "Until You Dead !");
 
 
 
