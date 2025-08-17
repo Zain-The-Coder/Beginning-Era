e6b415bda3bca8e781ff6fd14abcd9167268ebbb
Zain-The-Coder <zain015976@gmail.com>
2025-08-17 10:57:07 +0500

    Practising Javascript

--- Javascript/Events/app.js
+++ Javascript/Events/app.js
@@ -1,6 +1,31 @@
-function hello () {
-    console.log("Hello World");
+// function hello () {
+//     console.log("Hello World");
+// }
+
+// let x = document.getElementById("first-image");
+// let y = document.getElementById("second-image");
+
+// function changer() {
+//     x.src = "./Images/zakat.PNG" ;
+// }
+// function reverter () {
+//     y.src = "./Images/daily-food.PNG" ;
+//}
+
+// let x = document.getElementById("picture1");
+
+// function change ()  {
+//     x.src = "./Images/zakat.PNG" ;
+// }
+// function back () {
+//     x.src = "./Images/daily-food.PNG"  ;
+// }
+
+let change = document.getElementById("div");
+
+function ch () {
+    change.style.backgroundColor = "#f00" ;
+    change.style.height = "120px" ;
+    change.style.width = "120px" ;    
 }
-function changer () {
-    
-}
+
