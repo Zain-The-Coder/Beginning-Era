48d10f15ea04414b516025ca65833d3a3830a539
Zain-The-Coder <zain015976@gmail.com>
2025-08-24 01:15:42 +0500

    JS

--- Javascript/DOM/app.js
+++ Javascript/DOM/app.js
@@ -4,45 +4,118 @@
 
 // setInterval(moveForward, 800000);
 
-let sepcialPara = document.getElementsByClassName("special");
-let hello = document.getElementsByClassName("notSpecial");
+// let sepcialPara = document.getElementsByClassName("special");
+// let hello = document.getElementsByClassName("notSpecial");
 
-function changer () {
-    let isChanged = false ;
-    if(!isChanged) {
-    for(let i = 0 ; i < sepcialPara.length; i++) {
-        let para = sepcialPara[i] ;
-        para.style.backgroundColor = "black" ;
-        para.style.color = "white" ;
-        para.style.fontSize = "20px" ;
-        para.style.fontFamily = "monospace" ;
-        para.style.padding = "23px" ;
-        para.style.borderRadius = "20px"
-        isChanged = true ;
-        }
-    }  else  {
-    for(let i = 0 ; i < sepcialPara.length; i++) {
-        let paras = sepcialPara[i] ;
-        paras.style.backgroundColor = "red" ;
-        paras.style.color = "white" ;
-        paras.style.fontSize = "san-serif" ;
-        paras.style.borderRadius = "20px" ;
-        paras.style.padding = "23px" 
-        paras.style.fontSize = "20px" ;
-        isChanged = false ;
-    }
-    }
-}
-
-// function hel ()  {
-//     for(let i = 0 ; i < hello.length; i++) {
-//         let paras = hello[i] ;
+// function changer () {
+//     let isChanged = false ;
+//     if(!isChanged) {
+//     for(let i = 0 ; i < sepcialPara.length; i++) {
+//         let para = sepcialPara[i] ;
+//         para.style.backgroundColor = "black" ;
+//         para.style.color = "white" ;
+//         para.style.fontSize = "20px" ;
+//         para.style.fontFamily = "monospace" ;
+//         para.style.padding = "23px" ;
+//         para.style.borderRadius = "20px"
+//         isChanged = true ;
+//         }
+//     }  else  {
+//     for(let i = 0 ; i < sepcialPara.length; i++) {
+//         let paras = sepcialPara[i] ;
 //         paras.style.backgroundColor = "red" ;
 //         paras.style.color = "white" ;
 //         paras.style.fontSize = "san-serif" ;
 //         paras.style.borderRadius = "20px" ;
 //         paras.style.padding = "23px" 
 //         paras.style.fontSize = "20px" ;
+//         isChanged = false ;
+//     }
 //     }
 // }
-setTimeout(changer , 1000);
+
+// // function hel ()  {
+// //     for(let i = 0 ; i < hello.length; i++) {
+// //         let paras = hello[i] ;
+// //         paras.style.backgroundColor = "red" ;
+// //         paras.style.color = "white" ;
+// //         paras.style.fontSize = "san-serif" ;
+// //         paras.style.borderRadius = "20px" ;
+// //         paras.style.padding = "23px" 
+// //         paras.style.fontSize = "20px" ;
+// //     }
+// // }
+// setTimeout(changer , 1000);
+
+// let specialPara = document.getElementsByClassName("special");
+// let isChanged = true ;
+// setInterval(function hello () {
+//     if(isChanged) {
+//     for(let i = 0 ; i < specialPara.length; i++) {
+//         let para = specialPara[i] ;
+//         para.style.backgroundColor = "black" ;
+//         para.style.color = "white" ;
+//         para.style.fontSize = "20px" ;
+//         para.style.padding = "20px" ;
+//         para.style.fontFamily = "monospace" ;
+//         para.style.borderRadius = "20px"
+//         isChanged = false  ;
+//     }
+// }
+// else {
+//     for(let j = 0 ; j < specialPara.length ; j++) {
+//         let para = specialPara[j] ;
+//         para.style.color = "white" ;
+//         para.style.backgroundColor = "red" ;
+//         para.style.fontFamily = "monospace" ;
+//         para.style.fontSize = "20px" ;
+//         para.style.padding = "20px" ;
+//         para.style.borderRadius = "20px"
+//         isChanged = true ;
+//     }
+// }
+// }, 1000);
+
+// let notSpecial = document.getElementsByClassName("not");
+// let isClose = true ;
+// setInterval(function hi() {  
+//     if(isClose) {
+//     for(let i = 0 ; i < notSpecial.length; i++) {
+//         let extra = notSpecial[i] ;
+//         extra.style.backgroundColor = "yellow" ;
+//         extra.style.color = "black" ;
+//         extra.style.fontFamily = "monospace" ;
+//         extra.style.padding = "20px" ;
+//         extra.style.borderRadius = "20px" ;
+//         extra.style.fontSize = "20px" ;
+//         isClose = false ;
+//     } 
+// }   else {
+//         for(let j = 0 ; j < notSpecial.length ; j++) {
+//         let extra = notSpecial[j] ;
+//         extra.style.backgroundColor = "grey" ;
+//         extra.style.color = "blue" ;
+//         extra.style.fontFamily = "monospace" ;
+//         extra.style.padding = "20px" ;
+//         extra.style.borderRadius = "20px" ;
+//         extra.style.fontSize = "20px" ;
+//         isClose = true ;
+//     }
+// }
+    
+// }, 1000);
+
+// let curruntDate = new Date() ;
+// //let hour = String(curruntDate.getHours()).padStart(1 , "0") ;
+// let hour = curruntDate.getHours();
+// let minute = curruntDate.getMinutes() ;
+// let second = curruntDate.getSeconds() ;
+
+
+let curruntDate = new Date ();
+let hour = curruntDate.getHours() ;
+let minute = curruntDate.getMinutes() ;
+let second = curruntDate.getSeconds() ;
+function dater () {
+    
+}
