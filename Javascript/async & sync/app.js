54bb9a8dbae70bea8f4a01cbc76f73c965660e38
Zain-The-Coder <zain015976@gmail.com>
2025-09-20 17:04:49 +0500

    JS

---
+++ Javascript/async & sync/app.js
@@ -0,0 +1,40 @@
+// let arr = [23 , 53, 21, 123, 23 ];
+
+// console.log(arr);
+// let newArray = arr.sort((a , b) => a - b);
+// console.log(newArray)
+
+
+// function timer () {
+//     let myInterval = setInterval(() => {
+//         console.log("First Interval is Working....");
+//     }, 3000);
+//     let myInterval2 = setInterval(() => {
+//         console.log("Second Interval Is Working....");
+//     }, 3000);
+//     let myInterval3 = setInterval(() => {
+//         console.log("Third Interval Is Working....");
+        
+//     }, 3000);
+//     console.log(myInterval , myInterval2 , myInterval3)
+// }
+// timer() ;
+
+
+// function callBackFunction(email , password , callBack , secondCallBack){
+//     if(email === "zain015976@gmail.com" && password === "zain9.5.09") {
+//         setInterval( () => {
+//         callBack(secondCallBack()); 
+//         } , 3000) ;
+//     }
+// }
+
+// callBackFunction("zain015976@gmail.com" , "zain9.5.09" , 
+//     function callBack (secondCallBack) {
+//         console.log(secondCallBack)
+//     } ,
+//     function confusion () { 
+//         let x = "I Am Missing You Nowadays , Seriously !" ;
+//         return x ;
+//     }
+// )
