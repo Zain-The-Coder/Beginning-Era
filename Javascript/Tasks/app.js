485f43b31f5d32b1c154b1012c3de921eea4c2cf
Zain-The-Coder <zain015976@gmail.com>
2025-09-23 11:10:33 +0500

    Js

---
+++ Javascript/Tasks/app.js
@@ -0,0 +1,13 @@
+// let user = Number(prompt("enter your number"));
+// let limit = Number(prompt("enter youtr range of table"));
+
+// for(let i = 1 ; i <= limit ; i++) {
+//     console.log(user + " x " + i + " = " + user*i);
+// }
+
+let userNumber = Number(prompt("enter your number"));
+if(userNumber %  === 0) {
+    console.log("it is prime number");
+} else {
+    console.log("it is not a prime number");
+}
