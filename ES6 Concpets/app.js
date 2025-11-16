54a26da0d8fb3865458d5ea721c95514d3919dda
Zain-The-Coder <zain015976@gmail.com>
2025-11-16 11:17:37 +0500

    JS:

---
+++ ES6 Concpets/app.js
@@ -0,0 +1,16 @@
+// class Car {
+//     constructor(carName , carColor , carModel) {
+//         this.carName = carName ;
+//         this.carColor = carColor ;
+//         this.carModel = carModel ;
+//     }
+//     function engine () {
+//         console.log("Engine Has Been Started");
+//     }
+// }
+// let honda = new Car("Honda" , "Black" , "2025");
+// let totota = new Car("Toyota" , "Grey" , "2020");
+// console.log(honda, honda.engine);
+// console.log(totota)
+
+//console.log(3.555.toFixed(2))
